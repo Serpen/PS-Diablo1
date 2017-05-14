d4f114eb61465af9c951ca97a8dd69b505bbec4e
Marco <serpen@hotmail.de>
2017-05-14 11:05:32 +0200

    Structures

---
+++ Skeleton.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Serpen.Diablo
+{
+    class Spell {
+        public eSpell Spell { get; set; }
+        public byte Index { get; set; }
+        public String Spellbook { get; set; }
+        public bool Enabled { get; set; }
+        public byte Level { get; set; }
+    }
+}
